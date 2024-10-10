6af52fb5193c1ea7d245dfaea655348dc43635a8
jjhgfgthfdddx <manafalthaf01@gmail.com>
2024-10-10 16:16:45 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -67,9 +67,7 @@ async def start(client, message):
             
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)      
-        await message.reply_photo(
-            photo=random.choice(PICS),
-            caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
+        await message.reply_text(script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )
@@ -92,9 +90,7 @@ async def start(client, message):
             
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)      
-        await message.reply_photo(
-            photo=random.choice(PICS),
-            caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
+        await message.reply_text(script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )
