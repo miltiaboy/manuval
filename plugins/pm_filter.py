31dd6da8dd1670b64a858c0e7f359d25c38ef64b
miltiaboy <114646328+miltiaboy@users.noreply.github.com>
2025-02-12 17:51:59 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -44,12 +44,12 @@ EPISODES = ["E 01", "E 02", "E 03", "E 04", "E 05", "E 06", "E 07", "E 08", "E 0
 QUALITIES = ["360p", "480p", "720p", "1080p", "1440p", "2160p"]
 YEARS = ["1900", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999", "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022", "2023", "2024", "2025"]
 
-@Client.on_message(filters.text & filters.incoming)
+@Client.on_message(filters.group & filters.text & filters.incoming)
 async def give_filter(client, message):
-        await global_filters(client, message)
+    k = await manual_filters(client, message)
+    if k == False:
         await auto_filter(client, message)
-    
-        
+           
         
 @Client.on_callback_query(filters.regex(r"^next"))
 async def next_page(bot, query):
